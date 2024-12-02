ba1315bf1c81d030ca8671ed9b1fc345b45914a5
dct <2382056614@qq.com>
2024-12-03 00:07:49 +0800

    登入页面初始化

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/home/index.vue'
-
+import LoginView from '../views/login/index.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -9,6 +9,11 @@ const router = createRouter({
       name: 'home',
       component: HomeView,
     },
+    {
+      path: '/login',
+      name: 'loginView',
+      component: LoginView
+    }
   ],
 })
 
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,9 +3,57 @@
 </script>
 
 <template>
-<!-- login -->
+  <div class="login-container">
+    <div class="left-img">
+      <img src="https://ideaape.com/static/images/2_1.avif">
+    </div>
+    <div class="login-box">
+        <div class="slogan">
+          ONE TOUCH ONT WORLD !
+        </div>
+    </div>
+    <div class="right-img">
+      <img src="https://ideaape.com/static/images/neck.avif">
+    </div>
+  </div>
 </template>
 
 <style scoped>
+.login-container{
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+  background-color: #f0eae1 ;
+}
+.left-img img{
+  position: absolute;
+  left: 5vw;
+  top: 10vh;
+  width: 17vw;
+  height: 60vh;
+}
+.right-img img{
+  position: absolute;
+  right: 0;
+  width: 17vw;
+  height: 100vh;
+}
+.login-container .login-box{
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 800px;
+  height: 500px;
+  border-radius: 20px;
+  background-color: #094255;
+}
+.login-container .login-box .slogan{
+  width: 100%;
+  height: 60px;
+  margin-top: 100px;
+  font-size: 25px;
+  text-align: center;
+}
 
 </style>
