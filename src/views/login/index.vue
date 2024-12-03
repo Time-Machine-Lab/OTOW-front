85f855c3709487269b0d665b985bdf5652563275
dct <2382056614@qq.com>
2024-12-03 22:35:48 +0800

    sync

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -8,9 +8,9 @@
       <img src="https://ideaape.com/static/images/2_1.avif">
     </div>
     <div class="login-box">
-        <div class="slogan">
-          ONE TOUCH ONT WORLD !
-        </div>
+        <p class="slogan">
+          ONE TOUCH ONE WORLD !
+        </p>
     </div>
     <div class="right-img">
       <img src="https://ideaape.com/static/images/neck.avif">
@@ -48,12 +48,20 @@
   border-radius: 20px;
   background-color: #094255;
 }
-.login-container .login-box .slogan{
+
+.login-container .login-box .slogan {
   width: 100%;
   height: 60px;
-  margin-top: 100px;
+  margin-top: 60px;
   font-size: 25px;
   text-align: center;
+  font-family: "阿里妈妈灵动体 VF Thin", sans-serif;
+  font-weight: bold;
+  background: linear-gradient(90deg, #ff7e5f, #feb47b); /* 渐变背景 */
+  -webkit-background-clip: text; /* 仅对文本剪裁背景 */
+  -webkit-text-fill-color: transparent; /* 使文本本身透明，以便显示背景 */
+  line-height: 60px; /* 垂直居中文本 */
 }
+
 
 </style>
