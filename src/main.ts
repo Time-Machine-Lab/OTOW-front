6ccafdbb6b4b76ab1a1c80b393545b7a99ff5430
dct <2382056614@qq.com>
2024-12-07 00:08:29 +0800

    市场页面开发

--- src/main.ts
+++ src/main.ts
@@ -10,16 +10,17 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import { aliases, md } from 'vuetify/iconsets/md'
+
 const vuetify = createVuetify({
-   components,
-   directives,
-   icons: {
-        defaultSet: 'md',
-        aliases,
-        sets: {
-                md,
+        components,
+        directives,
+        icons: {
+                defaultSet: 'md',
+                aliases,
+                sets: {
+                        md,
+                },
         },
-   },
 })
 // Pinia
 import { createPinia } from 'pinia'
