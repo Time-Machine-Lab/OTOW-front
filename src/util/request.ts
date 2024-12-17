2ef5f47f3d776a32bccd95851c1bf355051da101
LisianthusLeaf <3106334435@qq.com>
2024-12-17 16:26:53 +0800

    跨域配置

--- src/util/request.ts
+++ src/util/request.ts
@@ -4,7 +4,7 @@ import { statusTextMap,errorStatusCodes,ignoreStatusCodes } from '@/util/statusC
 import {storage} from "@/util/storage";
 
 const request = axios.create({
-    baseURL: "http://119.3.234.15:9000",
+    baseURL: "/api",
     timeout: 10000,
 });
 
